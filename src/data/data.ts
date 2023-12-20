dee43bbef41b67c1d178a47e858cf3f0263975f5
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-12-20 15:55:04 +0700

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -41,7 +41,7 @@ export const myProjects = [
       "This is a personal branding project that I created using Next JS, Tailwind CSS, and Framer Motion.",
     image: "project-1.png",
     link: "https://ibrahimelgibran.com/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Portfolio%20Personal%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["Next JS", "Tailwind CSS", "Framer Motion"],
   },
   {
@@ -50,7 +50,7 @@ export const myProjects = [
       "This is a IegcodeTube is a youtube's clone website that i've created using React JS, Material UI and RapidAPI as data API for learning purposes.",
     image: "project-3.png",
     link: "https://iegcodetube.vercel.app/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Youtube%20Clone%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["React JS", "Material UI", "RapidAPI"],
   },
   {
@@ -59,7 +59,7 @@ export const myProjects = [
       "This is a Website Profile PPDB 2024-2025 project that I created using Codeigniter 3, Bootstrap, MySQL.",
     image: "project-2.png",
     link: null,
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Profil%20and%20PPDB%20Online%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["Codeigniter", "Bootstrap", "MySQL"],
   },
   {
@@ -68,7 +68,7 @@ export const myProjects = [
       "This is a CMS ESKAMUGA project that I created using PHP, Codeignither 4 and MySQL.",
     image: "project-4.png",
     link: null,
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20CMS%20School%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["Codeigniter", "Bootstrap", "MySQL"],
   },
   {
@@ -77,7 +77,7 @@ export const myProjects = [
       "This is a Management of Cource Student Data With System Dashbaord, CRUD and Tech Stack PHP Versi 8, MYSQL, CSS, BOOTSTRAP 5.",
     image: "project-5.png",
     link: null,
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Manajement%20Cource%20Data%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["PHPv8", "Bootstrap", "MySQL"],
   },
   {
@@ -86,7 +86,7 @@ export const myProjects = [
       "This is a personal branding project that I created using Tailwind CSS and Bootstap.",
     image: "project-6.png",
     link: "https://ibrahimelgibran.github.io/PersonalTemplate/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Portofolio%20Personal%20v2%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["Tailwind CSS", "Bootstrap"],
   },
   {
@@ -95,7 +95,7 @@ export const myProjects = [
       "This is a Implement an API fetch of your GitHub portfolio, show a page with a list of all your repositories on GitHub, and show another page linking to each repo using nested routes while using all the necessary tools in react.",
     image: "project-12.png",
     link: "https://iegcodegithub.vercel.app/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Github%20Clone%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["ReactJS", "Vanila CSS"],
   },
   {
@@ -104,7 +104,7 @@ export const myProjects = [
       "This is a personal branding project that I created using Tailwind CSS and Bootstap.",
     image: "project-7.png",
     link: "https://sotoijomonjali.com/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Profil%20Restaurant%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
   },
   {
@@ -113,7 +113,7 @@ export const myProjects = [
       "This is a CBT online SMK MUHAMMADIYAH GAMPING for students ESKAMUGA with own fiture complite, i created using tech stack Codeigniter 4, CSS, Bootstrap, JavaScript, MYSQL.",
     image: "project-9.png",
     link: null,
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20CBT%20Online%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["Codeigniter 4", "Bootstrap", "MYSQL"],
   },
   {
@@ -122,7 +122,7 @@ export const myProjects = [
       "This is a Web Wedding digital and many fiture complite like mp3, maps, photo, O'clock, chat live congratulations.",
     image: "project-8.png",
     link: "https://iegcodewedding.vercel.app/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Invite%20Wedding%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
   },
   {
@@ -131,7 +131,7 @@ export const myProjects = [
       "This is a Create Link in Bio page from my profile Instagram tech stack HTML, CSS and JavaScript.",
     image: "project-10.png",
     link: "https://iegcodelink.vercel.app/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Link%20In%20Bio%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
   },
   {
@@ -140,7 +140,7 @@ export const myProjects = [
       "TThis is a Web Wedding version 2 and own fiture mp3, O'clock, Maps, Save date, gallery photo, animation loading dll.",
     image: "project-11.png",
     link: "https://iegcodeweddingv2.vercel.app/",
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%20Elbran%20👋🏻%20,%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Invite%20Wedding%20v2%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga,%20fitur%20yang%20termasuk,%20dan%20proses%20kerjanya?%20%0A%0ATerima%20kasih!",
     techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
   },
 ];
